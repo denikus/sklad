d54a4491236f8cd52a218f172aac3ff8039889a2
denix <denis.soloshenko@gmail.com>
2014-02-18 00:07:19 +0200

    product migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140215225957) do
+ActiveRecord::Schema.define(version: 20140217214420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,9 +21,22 @@ ActiveRecord::Schema.define(version: 20140215225957) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "ancestry"
+    t.integer  "identifier"
   end
 
   add_index "categories", ["ancestry"], name: "index_categories_on_ancestry", using: :btree
+  add_index "categories", ["identifier"], name: "index_categories_on_identifier", using: :btree
+
+  create_table "products", force: true do |t|
+    t.integer  "identifier"
+    t.integer  "category_id"
+    t.string   "name"
+    t.string   "url"
+    t.string   "picture_url"
+    t.integer  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
